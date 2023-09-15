c385a703b99f67bbad1d91dff5ccdc1c79e9386d
hiakitoya <hiakitoya@gmail.com>
2023-09-15 20:20:24 -0300

    Agora ele clica no Cookie

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ class Cookie_game():
     def __init__(self):
         self.abrir_navegador()
         self.abrir_site('https://orteil.dashnet.org/cookieclicker/')
+        self.jogar()
         sleep(10)
 
+    def jogar(self):
+        while True:
+            self.clicar_no_cookie()
     def abrir_navegador(self):
         self.navegador = webdriver.Chrome()
 
@@ -16,4 +20,10 @@ class Cookie_game():
         #Escolher linguagem
         self.navegador.find_element('css selector', 'div#langSelect-PT-BR').click()   
 
+    def clicar_no_cookie(self):
+        for x in range(20):
+            try:self.navegador.find_element('css selector', 'button#bigCookie').click()
+            except Exception as E:
+                #print(E)
+                pass
 Cookie_game()
